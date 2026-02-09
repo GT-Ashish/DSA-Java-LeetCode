2dcbe756c6731cbf20a87d84e76bc4cd4d58ee27
Ashish Murarka <murarkaashish6294@gmail.com>
2026-02-09 23:16:11 +0530

    Time: 1 ms (88.38%) | Memory: 42.9 MB (95.64%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
@@ -1,16 +1,9 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        StringBuilder sb = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
-        for(String s : word1){
-            sb.append(s);
-        }
-        for(String s : word2){
-            sb2.append(s);
-        }
-        //to compare two string builders using toString()
-        //sb doesn't overide equals() due to hashmap logic breaking so its same as using ==
-        if(sb.toString().equals(sb2.toString())){
+        //use this to convert string array directly to string
+        String a = String.join("",word1);
+        String b = String.join("",word2);
+        if(a.equals(b)){
             return true;
         }
         return false;
