4ced9e797f90d212fb01dead91f6821b4411c9cc
Ashish Murarka <murarkaashish6294@gmail.com>
2026-02-11 23:32:46 +0530

    Time: 5 ms (66.11%) | Memory: 45.7 MB (71.59%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.java
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -4,17 +4,18 @@ class Solution {
         int y = 0;
         int len = moves.length();
         for(int i = 0; i < len; i++){
-            if(moves.charAt(i) == 'U'){
-                x++;
-            }
-            else if(moves.charAt(i) == 'D'){
-                x--;
-            }
-            else if(moves.charAt(i) == 'L'){
-                y--;
-            }
-            else{
-                y++;
+            switch(moves.charAt(i)){
+                case('U'):
+                    x++;
+                    break;
+                case('D'):
+                    x--;
+                    break;
+                case('L'):
+                    y--;
+                    break;
+                default : 
+                    y++;
             }
         }
         return x == 0 && y == 0;
