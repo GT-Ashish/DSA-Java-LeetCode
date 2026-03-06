229c280b95cea80cf5cf580b6e78f20023584187
Ashish Murarka <murarkaashish6294@gmail.com>
2026-03-06 22:54:21 +0530

    Time: 4 ms (94.44%) | Memory: 46.3 MB (68.57%) - LeetSync

--- 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
@@ -1,20 +1,17 @@
 class Solution {
     public int numSub(String s) {
+        //no n * n + 1 / 2 formula needed diretly adding.
         int len = s.length();
         int count = 0;
         long substring = 0;
         for(int i = 0; i < len; i++){
-            char ch = s.charAt(i);
-            if(ch == '1'){
+            if(s.charAt(i) == '1'){
                 count++;
+                substring += count;
             }
             else{
-                substring += ((long)count * (count + 1)) / 2 % 1000000007;
                 count = 0;
             }
-        }
-        if(count > 0){
-            substring += ((long)count * (count + 1)) / 2 % 1000000007;
         }
         return (int)(substring % 1000000007);
     }
