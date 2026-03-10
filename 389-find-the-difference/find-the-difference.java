2bc9fe92791a236492bfa0365a6014253cc8168a
Ashish Murarka <murarkaashish6294@gmail.com>
2026-03-10 23:53:13 +0530

    Time: 1 ms (99.84%) | Memory: 43 MB (84.53%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,18 +1,13 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        int [] array1 = new int[26];
-        int [] array2 = new int[26];
+        //using ascii sum
         int len = s.length();
+        int sum1 = 0;
+        int sum2 = t.charAt(len);
         for(int i = 0; i < len; i++){
-            array1[s.charAt(i) - 'a'] += 1;
-            array2[t.charAt(i) - 'a'] += 1;
+            sum1 += s.charAt(i);
+            sum2 += t.charAt(i);
         }
-        array2[t.charAt(len) - 'a'] += 1;
-        for(int i = 0; i < 26; i++){
-            if(array1[i] != array2[i]){
-                return (char)(i + 'a');
-            }
-        }
-        return 'a';
+        return (char)(sum2 - sum1);
     }
 }
