d47d9cc1d247dc095df19529caf4fd48d571f116
Ashish Murarka <murarkaashish6294@gmail.com>
2026-03-13 20:30:05 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (58.02%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,26 +1,25 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int white = 0;
-        int red = 0;
-        for(int element : nums){
-            if(element == 0){
-                red++;
+        // using dutch national flag algo
+        int start = 0;
+        int middle  = 0;
+        int end = nums.length - 1;
+        while(middle <= end){
+            //while swapping with start the value will always be one as in the region between start and mid there are all ones so we increment mid while swapping with 1;
+            if(nums[middle] == 0){
+                    nums[middle] = 1;
+                    nums[start] = 0;
+                    start++;
+                    middle++;
             }
-            else if(element == 1){
-                white++;
-            }
-        }
-        white += red;
-        int len = nums.length;
-        for(int i = 0; i < len; i++){
-            if(i < red){
-                nums[i] = 0;
-            }
-            else if(i >= red && i < white){
-                nums[i] = 1;
+            //while swapping with end we don't know what the value the end will contain hence we dont move mid and check it again;
+            else if(nums[middle] == 2){
+                    nums[middle] = nums[end];
+                    nums[end] = 2;
+                    end--;
             }
             else{
-                nums[i] = 2;
+                middle++;
             }
         }
     }
